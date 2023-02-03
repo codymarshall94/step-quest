a4843d32f2f210088b8547db7f9eec23e0ac8ca2
Cody Marshall <codymstuntsx@gmail.com>
2023-02-02 17:10:17 -0800

    json of enemy data

---
+++ src/enemies.js
@@ -0,0 +1,30 @@
+export const ENEMIES = [
+    {
+        name: 'Acid Ooze',
+        health: 10,
+        attack: 2,
+        defense: 1,
+        image: require('./assets/images/enemies/AcidOozeIdleSide.gif'),
+    },
+    {
+        name: 'Brawny Ogre',
+        health: 15,
+        attack: 3,
+        defense: 2,
+        image: require('./assets/images/enemies/BrawnyOgreIdleSide.gif'),
+    },
+    {
+        name: 'Storm Giant',
+        health: 20,
+        attack: 4,
+        defense: 3,
+        image: require('./assets/images/enemies/StormGiantIdleSide.gif'),
+    },
+    {
+        name: 'Werewolf Stalker',
+        health: 25,
+        attack: 5,
+        defense: 4,
+        image: require('./assets/images/enemies/WerewolfStalkerIdleSide.gif'),
+    },
+];
