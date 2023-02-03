e43f0d8fabd1df462667a99c7b81950dbbff8027
Cody Marshall <codymstuntsx@gmail.com>
2023-02-02 17:10:51 -0800

    new routes for different pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
-import Login from "./pages/Login";
+import Adventure from "./pages/Adventure/Adventure";
+import Allocate from "./pages/Allocate/Allocate";
+import Login from "./pages/Landing/Login";
+import Main from "./pages/Main/Main";
 
 function App() {
+
+  /* formula for experience points
+
+  const ExperienceForLevel = (level) => {
+  let total = 0;
+  for (let i = 1; i < level; i++) {
+    total += Math.floor(i + 300 * Math.pow(2, i / 7.0));
+  }
+
+  return Math.floor(total / 4);
+}
+*/
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<RegisterForm />} />
+        <Route path="/main" element={<Main /> } />
+        <Route path="/allocate" element={<Allocate />} />
+        <Route path="/adventure" element={<Adventure />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </div>
   );
